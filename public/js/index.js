ec11698660c84e3e8d4184e1bc4e001ac21ccf20
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2017-03-27 02:06:14 -0400

    Push

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
+var mjpeg_img;
 $(document).ready(function() {
 	init_joystick();
-
+	mjpeg_img = document.getElementById("mjpeg_dest");
 });
-var mjpeg_img;
  
 function reload_img () {
 	var host_name = location.hostname;
@@ -12,7 +12,6 @@ function error_img () {
   setTimeout("mjpeg_img.src = 'cam_pic.php?time=' + new Date().getTime();", 100);
 }
 function init() {
-  mjpeg_img = document.getElementById("mjpeg_dest");
   mjpeg_img.onload = reload_img;
   mjpeg_img.onerror = error_img;
   reload_img();
