207135270fa42685bfbc1a4c77f5bf7e22219583
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2017-03-27 01:39:20 -0400

    Send json requests when an actual change has happened. So we don't flood the tiny pi server.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var body_parser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(body_parser.json());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/joystick_update', function (request, response) {
+  if (!request.body) {
+  	return response.sendStatus(400);
+  }
+  console.log(request.body);
+  response.sendStatus(200);
 });
 
 app.listen(3000, function () {
--- public/js/index.js
+++ public/js/index.js
@@ -6,30 +6,61 @@ $(document).ready(function() {
 
 function init_joystick() {
 	console.log("touchscreen is", VirtualJoystick.touchScreenAvailable() ? "available" : "not available");
+	window.Updater = new Updater();
 	var joystick	= new VirtualJoystick({
 		container	: document.getElementById('container'),
 		mouseSupport	: true,
 	});
 	joystick.addEventListener('touchStart', function(){
-		console.log('down');
+		console.log('Touch down');
 	});
 	joystick.addEventListener('touchEnd', function(){
-		console.log('up');
-	});
-	joystick.addEventListener('mousedown', function(){
-		console.log('down');
-	});
-	joystick.addEventListener('mouseup', function(){
-		console.log('up');
+		console.log('Touch up');
 	});
 	setInterval(function(){
-		var outputEl	= document.getElementById('position');
-		outputEl.innerHTML	= ''
-			+ ' dx:'+joystick.deltaX()
-			+ ' dy:'+joystick.deltaY()
+		var delta_x = joystick.deltaX();
+		var delta_y = joystick.deltaY();
+		Updater.handle_update(delta_x, delta_y);
+		var update 	= ''
+			+ ' dx:'+delta_x
+			+ ' dy:'+delta_y
 			+ (joystick.right()	? ' right'	: '')
 			+ (joystick.up()	? ' up'		: '')
 			+ (joystick.left()	? ' left'	: '')
 			+ (joystick.down()	? ' down' 	: '')	
-	}, 1/30 * 1000);
+			// console.log(update)
+	}, 1/20 * 1000);
+};
+
+
+var Updater = function() {
+	this.last_x = 0;
+	this.last_y = 0;
+	this.buffer = 1;
+	this.end_point = 'joystick_update';
+};
+
+Updater.prototype.handle_update = function(delta_x, delta_y) {
+	if (delta_x != this.last_x || delta_y != this.last_y) {
+		console.log("Handling Change");
+		this.last_x = delta_x;
+		this.last_y = delta_y;
+		var new_update = {
+			x: this.last_x,
+			y: this.last_y
+		}	
+		$.ajax({
+			type: "POST",
+			url: this.end_point,
+			data: JSON.stringify(new_update),
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json'
+		});
+		// $.post(this.end_point, new_update);
+	}
+	else {
+		// No change
+		console.log("Not Handling");
+		return;
+	}
 };
